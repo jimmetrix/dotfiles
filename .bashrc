16e9310ad66634fb59b9871462b18869961b1615
jimmetrix <jmtrx@proton.me>
2025-05-11 06:32:22 +0300

    added lots of shit to waybar config

--- .bashrc
+++ .bashrc
@@ -1,2 +1,3 @@
 alias l='ls -a --color=auto'
+/usr/bin/fastfetch
 PS1="\d \t \w> "
--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -241,6 +241,7 @@ require("lazy").setup({
 	-- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
 	"tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
 	"tpope/vim-endwise",
+	"hrsh7th/nvim-cmp",
 	-- NOTE: Plugins can also be added by using a table,
 	-- with the first argument being the link and the following
 	-- keys can be used to configure plugin behavior/loading/etc.
