2186ada037d9a182828adfc09eccb8bd25b4b7ef
jimmetrix <jmtrx@proton.me>
2025-05-13 16:07:18 +0300

    finish up base waybar config and styling as well as add empty shell files to be edited

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -351,6 +351,14 @@ require("lazy").setup({
 	--
 	-- Use the `dependencies` key to specify the dependencies of a particular plugin
 
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = true,
+		-- use opts = {} for passing setup options
+		-- this is equivalent to setup({}) function
+	},
+
 	{ -- Fuzzy Finder (files, lsp, etc)
 		"nvim-telescope/telescope.nvim",
 		event = "VimEnter",
