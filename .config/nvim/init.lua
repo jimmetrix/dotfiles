10be44ec858d159a7c15acc0055c70cf38c1464f
jimmetrix <jmtrx@proton.me>
2025-05-25 18:35:53 +0300

    add oil.nvim to init lua, remove bfetch directory

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -550,6 +550,18 @@ require('lazy').setup({
       },
       indent = { enable = true, disable = { 'ruby' } },
     },
+
+    {
+      'stevearc/oil.nvim',
+      ---@module 'oil'
+      ---@type oil.SetupOpts
+      opts = {},
+      -- Optional dependencies
+      -- dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+      dependencies = { 'nvim-tree/nvim-web-devicons' }, -- use if you prefer nvim-web-devicons
+      -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+      lazy = false,
+    },
   },
 
   {
